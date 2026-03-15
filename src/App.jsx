24c401c5be5c5f56e316d987b3247df2333fab9e
Hanif <mohammadabuhanif95@gmail.com>
2026-03-15 17:10:55 +0800

    React setup for making world to go first react project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Countries from "./components/Countries/Countries";
+
+const App = () => {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  );
+};
+
+export default App;
